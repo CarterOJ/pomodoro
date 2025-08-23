b1dc53f6c455fa6d62da7c2886e69e0a78e77568
Carter Johnson <carterojohnson@gmail.com>
2025-08-23 03:31:34 -0600

    Removed hacky prod super user addition

--- backend/users/apps.py
+++ backend/users/apps.py
@@ -1,16 +1,5 @@
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
-import os
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
-
-    def ready(self):
-        User = get_user_model()
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser(
-                username=os.getenv("ADMIN_USERNAME"),
-                email=os.getenv("ADMIN_EMAIL"),
-                password=os.getenv("ADMIN_PASSWORD")
-            )
